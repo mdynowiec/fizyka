504f0ad99d3613c5589401a02bbb8315f320c22e
Maciej Dynowiec <131781055+mdynowiec@users.noreply.github.com>
2023-12-03 23:38:23 +0100

    XDDDD

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+function addListeners(){
+
+    let element = document.querySelectorAll('tbody tr');
+
+    element.forEach(e => {
+
+        e.addEventListener('click', t => {
+            document.querySelector('.info').classList.toggle('show');
+        })
+
+    })
+    
+}
+
+function hide(){
+
+    document.querySelector('.info').classList.remove('show')
+
+}
